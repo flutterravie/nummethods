6aceb581603ac246dfcc2c9e99c213d6a0cf2ae7
Anton Bersenyov <flutterravie@gmail.com>
2015-03-29 14:42:08 +0500

    upd

--- app/scripts/lab2.js
+++ app/scripts/lab2.js
@@ -2,7 +2,8 @@ $(function () {
 	var numArray = [],
 		colCount = 5,
 		rowCount = 4,
-		x = [];
+		x = [],
+		maxB = 0;
 
 	for (var i = 0; i < colCount; i++) {
 		numArray[i] = [];
@@ -15,8 +16,19 @@ $(function () {
 		for (var i = 0; i < colCount; i++) {
 			for (var k = 0; k < rowCount; k++) {
 				numArray[i][k] = parseFloat($('input[data-col=' + i + '][data-row=' + k + ']').val());
-				console.log(numArray[i][k]);
+				// console.log(numArray[i][k]);
 			}
+		}
+
+		// проверка сходимости метода итераций
+
+		for (var i = 3; i < rowCount; i++) {
+		var tempSum = 0;
+			for (var k = 0; k < (colCount-1); k++) {
+				tempSum = tempSum + Math.abs(numArray[k][i]);
+			}
+			if (tempSum > maxB) { maxB = tempSum; }
+			console.log(maxB);
 		}
 
 		for (var i = 0; i < colCount; i++) {
