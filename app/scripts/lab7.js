cd976cecc61d8eccd75195d410f7b17ccbd9ae3d
Anton Bersenyov <flutterravie@gmail.com>
2015-03-31 16:41:34 +0500

    add another example for lab7

--- app/scripts/lab7.js
+++ app/scripts/lab7.js
@@ -16,6 +16,7 @@ $(function () {
 
 		for (var i = 1; i <= ((upLimit - downLimit) / step); i++) {
 			subSum = Math.abs(downLimit + (step * i) - (step / 2)); // находим сумму при определённом i
+			// subSum = 1/(1 - (downLimit + (step * i) - (step / 2))); // функция из примера
 			sum = sum + subSum; // прибавляем её к площади
 			$('.js-sum').append('<span>' + subSum + '</span>');
 			if (i < ((upLimit - downLimit) / step)) {
@@ -23,7 +24,7 @@ $(function () {
 			};
 		}
 
-		sum = sum * step;
+		sum = (sum * step).toFixed(5);
 		$('.js-sum').append('<span>) = ' + sum + '</span>');
 	});
 });
